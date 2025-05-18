724d126cd781bae4311a0112d01ab8b2b328efb8
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-05-18 22:36:49 +0700

    Shadcn UI Setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <Button>Let's build Dev Ngyxn Udemy</Button>
+  )
+}
+
+export default App
